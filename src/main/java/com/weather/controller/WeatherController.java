b3b4f73eb4e500c3206e7bbcf4291d3a8d4d7b2a
facu sacchi <faq.ndo.n.s@gmail.com>
2023-11-20 13:27:12 -0300

    added cross origins for all domains in controller

--- src/main/java/com/weather/controller/WeatherController.java
+++ src/main/java/com/weather/controller/WeatherController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.weather.service.WeatherService;
 
 @RestController
 @RequestMapping("/api/v1/weather")
+@CrossOrigin("*")
 public class WeatherController {
 
 	private WeatherService weatherService;
